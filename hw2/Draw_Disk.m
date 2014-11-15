5f3c942a4f39cf810e2aacd74000e455a81f2092
Sean Prasertsit <sean@prasertsit.com>
2014-11-14 16:32:11 -0800

    Finished hw3

--- hw2/Draw_Disk.m
+++ hw2/Draw_Disk.m
@@ -1,10 +1,14 @@
-function outvar=DrawDisk(x,y,r)
+function outvar=DrawDisk(x,y,a,b,r)
 n=100;
 for i=1:n
     theta=i*(2*pi/n);
     X(i)=x+r*cos(theta);
     Y(i)=y+r*sin(theta);
+    A(i)=a+r*cos(theta);
+    B(i)=b+r*sin(theta);
 end
 fill(X,Y,'r'); % fills (in red 'r') the 2-D polygon defined by vectors X and Y
-
+hold on;
+fill(A,B,'b');
+hold off;
 end
--- hw2/ball3d.m
+++ hw2/ball3d.m
@@ -3,64 +3,98 @@ clear all;
 clc;
 
 % Variables
-v_vector_red = [.3 0];
-v_vector_blue = [0, 0];
+v_vector_red = [5 0];
+v_vector_blue = [0 0];
 v_old_red = v_vector_red;
 v_old_blue = v_vector_blue;
 t = 0;
 t_final = 5;
 delta_t = .03;
 radius = .05;
-d_vector_red = [.5 1-radius];
-d_vector_blue = [.3 2-radius];
+d_vector_red = [.2 1];
+d_vector_blue = [.8 1.05];
 n_damp = .8;
 t_damp = .99;
 ball_collision = 0;
 
-x_wall = 1;
-y_wall = 2;
+L = 1;
+
+x_wall = L;
+y_wall = 2*L;
 
 clf;
 
+set(gcf, 'Units', 'normalized');
+set(gcf, 'Position', [0 0 .25 1]);
+
 while t < t_final,
+    % Get that last timestamp in
     if (t + delta_t > t_final),
         delta_t = t_final - t;
     end;
     % Store vectors at collision
     v_old_red = v_vector_red;
-    d_old = d_vector_red;
+    v_old_blue = v_vector_blue;
+    d_old_red = d_vector_red;
+    d_old_blue = d_vector_blue;
     % Update position
     d_vector_red = d_vector_red + v_vector_red*delta_t;
     d_vector_blue = d_vector_blue + v_vector_blue*delta_t;
     
-    if (sqrt((d_vector_blue(1) - d_vector_red(1)^2 + (d_vector_blue(2) - d_vector_red(2)))^2) <= 2*radius),
+    % Check for ball on ball action
+    if ( sqrt((d_vector_blue(1) - d_vector_red(1))^2 + (d_vector_blue(2) - d_vector_red(2))^2) <= 2*radius),
         ball_collision = 1;
+        disp('col');
     else
         ball_collision = 0;
     end;
     
+    % If there's a ball on ball collision
     if (ball_collision == 1),
+        
+        % Move to collision
         d_vector_red = d_old_red;
         d_vector_blue = d_old_blue;
-        delta_t = abs(1 - d_vector_red(1) - radius)/v_old_red(1);
+        
+        % Pre-calculate distances
+        d_vector_x = d_vector_red(1) - d_vector_blue(1);
+        d_vector_y = d_vector_red(2) - d_vector_blue(2);
+        v_vector_x = v_vector_red(1) - v_vector_blue(1);
+        v_vector_y = v_vector_red(2) - v_vector_blue(2); 
+        
+        % Find new delta_t of collision
+        delta_t = (sqrt((d_vector_x)^2 + (d_vector_y)^2) - 2*radius) / (sqrt((v_vector_x)^2 + (v_vector_y)^2));
+        
+        % Update position with new time
         d_vector_red = d_vector_red + v_old_red*delta_t;
         d_vector_blue = d_vector_blue + v_old_blue*delta_t;
         
+        % Calculate angle
         phi = atan((d_vector_blue(2) - d_vector_red(2))/(d_vector_blue(1) - d_vector_red(1)));
         
-        n_red = cos(phi)*d_vector_red(1) + sin(phi)*d_vector_red(2);
-        t_red = -sin(phi)*d_vector_red(1) + cos(phi)*d_vector_red(2);
+        % Get the normal and tangential velocities
+        n_red = cos(phi)*v_vector_red(1) + sin(phi)*v_vector_red(2);
+        t_red = -sin(phi)*v_vector_red(1) + cos(phi)*v_vector_red(2);
         
-        n_blue = cos(phi)*d_vector_red(1) + sin(phi)*d_vector_red(2);
-        t_blue = -sin(phi)*d_vector_red(1) + cos(phi)*d_vector_red(2);
+        n_blue = cos(phi)*v_vector_blue(1) + sin(phi)*v_vector_blue(2);
+        t_blue = -sin(phi)*v_vector_blue(1) + cos(phi)*v_vector_blue(2);
+        
+        % Convert back to regular velocity and swap normals
+        v_vector_red(1) = cos(phi)*n_blue - sin(phi)*t_red;
+        v_vector_red(2) = sin(phi)*n_blue + cos(phi)*t_red;
+    
+        v_vector_blue(1) = cos(phi)*n_red - sin(phi)*t_blue;
+        v_vector_blue(2) = sin(phi)*n_red + cos(phi)*t_blue;
+        
     end;
+    
         
     % Check if ball passes x walls
     if (d_vector_red(1) + radius >= x_wall),
         % Move to collision
-        d_vector_red = d_old;
+        d_vector_red = d_old_red;
         % Check time to collision
-        delta_t = abs(1 - d_vector_red(1) - radius)/v_old_red(1);
+        delta_t = abs(x_wall - d_vector_red(1) - radius)/v_old_red(1);
         % Update position again to true position
         d_vector_red = d_vector_red + v_old_red*delta_t;
         % Reverse direction
@@ -70,7 +104,7 @@ while t < t_final,
         disp('x');
     end;
     if (d_vector_red(1) <= radius),
-        d_vector_red = d_old;
+        d_vector_red = d_old_red;
         delta_t = abs(radius - d_vector_red(1))/v_old_red(1);
         d_vector_red = d_vector_red + v_old_red*delta_t;
         v_vector_red(1) = -1*v_old_red(1);
@@ -78,19 +112,51 @@ while t < t_final,
     end;
     % Check if ball passes y walls
     if (d_vector_red(2) + radius >= y_wall),
-        d_vector_red = d_old;
-        delta_t = abs(1 - d_vector_red(2) - radius)/v_old_red(2);
+        d_vector_red = d_old_red;
+        delta_t = abs(y_wall - d_vector_red(2) - radius)/v_old_red(2);
         d_vector_red = d_vector_red + v_old_red*delta_t;
         v_vector_red(2) = -1*v_old_red(2);
         v_vector_red = v_vector_red .* [t_damp n_damp];
         disp('y');
     end;
     if (d_vector_red(2) <= radius),
-        d_vector_red = d_old;
+        d_vector_red = d_old_red;
         delta_t = abs(radius - d_vector_red(2))/v_old_red(2);
         d_vector_red = d_vector_red + v_old_red*delta_t;
         v_vector_red(2) = -1*v_old_red(2);
         v_vector_red = v_vector_red .* [t_damp n_damp];
+    end;
+    % Blue ball walls
+    if (d_vector_blue(1) + radius >= x_wall),
+        d_vector_blue = d_old_blue;
+        delta_t = abs(x_wall - d_vector_blue(1) - radius)/v_old_blue(1);
+        d_vector_blue = d_vector_blue + v_old_blue*delta_t;
+        v_vector_blue(1) = -1*v_old_blue(1);
+        v_vector_blue = v_vector_blue .* [n_damp t_damp];
+        disp('xblue');
+    end;
+    if (d_vector_blue(1) <= radius),
+        d_vector_blue = d_old_blue;
+        delta_t = abs(radius - d_vector_blue(1))/v_old_blue(1);
+        d_vector_blue = d_vector_blue + v_old_blue*delta_t;
+        v_vector_blue(1) = -1*v_old_blue(1);
+        v_vector_blue = v_vector_blue .* [n_damp t_damp];
+    end;
+    % Check if ball passes y walls
+    if (d_vector_blue(2) + radius >= y_wall),
+        d_vector_blue = d_old_blue;
+        delta_t = abs(y_wall - d_vector_blue(2) - radius)/v_old_blue(2);
+        d_vector_blue = d_vector_blue + v_old_blue*delta_t;
+        v_vector_blue(2) = -1*v_old_blue(2);
+        v_vector_blue = v_vector_blue .* [t_damp n_damp];
+        disp('y');
+    end;
+    if (d_vector_blue(2) <= radius),
+        d_vector_blue = d_old_blue;
+        delta_t = abs(radius - d_vector_blue(2))/v_old_blue(2);
+        d_vector_blue = d_vector_blue + v_old_blue*delta_t;
+        v_vector_blue(2) = -1*v_old_blue(2);
+        v_vector_blue = v_vector_blue .* [t_damp n_damp];
     end;
     
     % Advance time
@@ -100,8 +166,7 @@ while t < t_final,
     delta_t = .03;
     
     % Draw
-    Draw_Disk(d_vector_red(1),d_vector_red(2),radius);
-    Draw_Blue(d_vector_blue(1),d_vector_blue(2),radius);
+    Draw_Disk(d_vector_red(1),d_vector_red(2),d_vector_blue(1),d_vector_blue(2),radius);
     axis([0 x_wall 0 y_wall]);
     drawnow;
     
