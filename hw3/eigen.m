bde9f1b11aa9217b505ba9bff6193e2e459c7954
Sean Prasertsit <seanp.usa@gmail.com>
2014-11-20 14:14:04 -0800

    Commented code

--- hw3/eigen.m
+++ hw3/eigen.m
@@ -3,6 +3,7 @@ clear all;
 clc;
 clf;
 
+% Mass and spring values
 m1 = 40;
 m2 = 20;
 m3 = 30;
@@ -11,6 +12,7 @@ k2 = 100;
 k3 = 250;
 k4 = 110;
 
+% k/m
 aa = (k1 + k2)/m1;
 ab = -k1/m1;
 ac = 0;
@@ -21,19 +23,24 @@ ca = 0;
 cb = -k3/m3;
 cc = k3/m3;
 
+% Matrix consisting of k/m values
 A = [aa ab ac; ba bb bc; ca cb cc];
+% Find eigenvectors and eignvalues
 [v,d] = eig(A);
 
+% Find frequencies
 freqs = Freq(d);
 
+% Set time for plots
 t_final = 5;
 dt = 0.02;
 t = 0:dt:t_final;
 
-x = v(1,1)*sin(freqs(1)*t);
+% Calculate sine plot
+x = v(2,1)*sin(freqs(1)*t);
 
-figure;
+% Plot
 plot(t, x);
 axis([0 5 -1 1]);
 xlabel('Time');
-title('Vibrating System');
+title('Vibrating System')
